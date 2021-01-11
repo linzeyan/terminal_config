8e75973798ae7330fc9a8036a2a025d04ebaf3f4
linzeyan <linzeyan>
2021-01-11 13:42:25 +0800

    [macos]: Update script

--- macos/iterm2-zmodem/iterm2-send-zmodem.sh
+++ macos/iterm2-zmodem/iterm2-send-zmodem.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-# 这个脚本来自 github，删掉了一些 ** 言论。
+# Author: Matt Mastracci (matthew@mastracci.com)
+# AppleScript from http://stackoverflow.com/questions/4309087/cancel-button-on-osascript-in-a-bash-script
+# licensed under cc-wiki with attribution required
+# Remainder of script public domain
 
-osascript -e 'tell application "iTerm2" to version' > /dev/null 2>&1 && NAME=iTerm2 || NAME=iTerm
+osascript -e 'tell application"iTerm2"to version' > /dev/null 2>&1 && NAME=iTerm2 || NAME=iTerm
 if [[ $NAME = "iTerm" ]]; then
-	FILE=`osascript -e 'tell application "iTerm" to activate' -e 'tell application "iTerm" to set thefile to choose file with prompt "Choose a file to send"' -e "do shell script (\"echo \"&(quoted form of POSIX path of thefile as Unicode text)&\"\")"`
+	FILE=$(osascript -e 'tell application"iTerm"to activate' -e 'tell application"iTerm"to set thefile to choose file with prompt"Choose a file to send"' -e "do shell script (\"echo \"&(quoted form of POSIX path of thefile as Unicode text)&\"\")")
 else
-	FILE=`osascript -e 'tell application "iTerm2" to activate' -e 'tell application "iTerm2" to set thefile to choose file with prompt "Choose a file to send"' -e "do shell script (\"echo \"&(quoted form of POSIX path of thefile as Unicode text)&\"\")"`
+	FILE=$(osascript -e 'tell application "iTerm2" to activate' -e 'tell application "iTerm2" to set thefile to choose file with prompt "Choose a file to send"' -e "do shell script (\"echo \"&(quoted form of POSIX path of thefile as Unicode text)&\"\")")
 fi
 if [[ $FILE = "" ]]; then
 	echo Cancelled.
@@ -18,5 +21,5 @@ else
 	/usr/local/bin/sz "$FILE" --escape --binary --bufsize 4096
 	sleep 1
 	echo
-	echo \# Received $FILE
+	echo \# Received"$FILE"
 fi
